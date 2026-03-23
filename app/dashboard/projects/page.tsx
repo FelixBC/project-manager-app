db34f6b8760256e5f578fd863695f50d97794be8
Felix J. Blanco C. <Felixblancocabrera@gmail.com>
2026-03-22 23:57:20 -0400

    feat: basic form for Projects page

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import FormActions from "@/components/form-actions";
 type projectsProps = {};
 
 const Projects: React.FC<projectsProps> = ({}) => {
+  const [name, setName] = useState<string>("");
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
   return (
-    <section>
-      <h1>Projects</h1>
-      <FormActions />
-    </section>
+    <form>
+      <section>
+        <h1>Projects</h1>
+        <label htmlFor="name">Name</label>
+        <input value={name} id="name" name="name" onChange={handleName} />
+        <FormActions />
+      </section>
+    </form>
   );
 };
 export default Projects;
